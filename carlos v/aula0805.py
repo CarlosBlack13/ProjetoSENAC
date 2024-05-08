c90ed44a3d321ec59a52e4eb30bca0e785769b39
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2024-05-08 17:28:59 -0300

    atualizar

--- carlos v/aula0805.py
+++ carlos v/aula0805.py
@@ -1,4 +1,4 @@
-a=[ 1, 2, 3]
+'''a=[ 1, 2, 3]
 a.append(4)
 a.insert(1, 5)
 print(a)
@@ -6,4 +6,11 @@ a.remove(1)
 print(a)
 
 ultimo_elemento = a.pop(1)
-print(ultimo_elemento)
+print(ultimo_elemento)'''
+
+lista = [1, 2, 3, 4, 5]
+lista2 = int(input("Digita um número intairo: "))
+lista.append(lista2)
+lista1 = str(input("Digite uma palavra: "))
+lista.insert(0, lista1)
+print(lista)
